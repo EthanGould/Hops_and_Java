0dcfff738bbec2cc2e7de7c167deda5539125cef
Ethan Gould <ethan.g.907@gmail.com>
2014-08-20 22:47:20 -0400

    new columns in breweries

--- HopsNJava/db/schema.rb
+++ HopsNJava/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820001048) do
+ActiveRecord::Schema.define(version: 20140821023201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,11 +19,8 @@ ActiveRecord::Schema.define(version: 20140820001048) do
   create_table "breweries", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "brewery_apis", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.text     "name"
+    t.text     "brewery_api_id"
   end
 
 end
