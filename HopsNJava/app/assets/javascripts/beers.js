1980cf65886118dc68f10d6405b469b060630f00
Ethan Gould <ethan.g.907@gmail.com>
2014-08-23 18:44:42 -0400

    ajax request now includes brewery name

--- HopsNJava/app/assets/javascripts/beers.js
+++ HopsNJava/app/assets/javascripts/beers.js
@@ -8,44 +8,58 @@ App.getRandomBeer = function() {
     var name,
         description,
         type,
-        image
+        image,
+        beerId
 
     name = response.data.name;
     description = response.data.style.description;
+    beerId = response.data.id
 
-    $('.beer--name').html(name);
-    $('.beer--description').html(description);
+    $('.beer__name').html(name);
+    $('.beer__description').html(description);
 
     // beer style/type
     if( response.data.style.name ){
       type = response.data.style.name;
-      $('.beer--type').html(type);
+      $('.beer__type').html(type);
     } else {
-      $('.beer--type').html('Unavailable');
+      $('.beer__type').html('Unavailable');
     };
 
     // beer image
     if (response.data.labels) {
       if( response.data.labels.medium ){
         image = response.data.labels.medium;
-        $('.beer--image').attr('src', image);
+        $('.beer__image').attr('src', image);
       } else if (response.data.labels.icon){
         image = response.data.labels.icon;
-        $('.beer--image').attr('src', image);
+        $('.beer__image').attr('src', image);
       } else if (response.data.labels.large){
         image = response.data.labels.large
-        $('.beer--image').attr('src', image);
+        $('.beer__image').attr('src', image);
       } else {
         image = 'http://img1.wikia.nocookie.net/__cb20140409232122/simpsons/images/0/03/Duff_logo.gif'
-        $('.beer--image').attr('src', image);
+        $('.beer__image').attr('src', image);
       };
     };
-
+    $.get('http://api.brewerydb.com/v2/beer/'+beerId+'/breweries?key=819b17a216351db6794aaf097f40c86a', function(response) {
+      console.log(response);
+      var brewery = response.data[0].name;
+      $('.beer__brewery').html(brewery);
+    });
   });
 }
 
-$(document).ready(function(){
-  $(".click-me").click(App.getRandomBeer)
+App.getBreweryPage = function(){
+  $.get('http://api.brewerydb.com/v2/beer/'+beerId+'/breweries?key=819b17a216351db6794aaf097f40c86a', function(response) {
+    console.log(response);
+    var brewery = response.data[0].name;
+    $('.beer__brewery').html(brewery);
+  });
+};
 
+$(document).ready(function(){
+  $(".click-me").click(App.getRandomBeer);
+  $(".beer__brewery").click(App.getBreweryPage);
 });
 
