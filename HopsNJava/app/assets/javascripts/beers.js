07bc491a68d217c29c8c6ca72b8e4d2b6edece8a
Ethan Gould <ethan.g.907@gmail.com>
2014-08-24 12:50:09 -0400

    made functions to build random beer and show page beer

--- HopsNJava/app/assets/javascripts/beers.js
+++ HopsNJava/app/assets/javascripts/beers.js
@@ -4,7 +4,7 @@ App.getRandomBeer = function() {
 
   $.get('http://api.brewerydb.com/v2/beer/random/?key=819b17a216351db6794aaf097f40c86a', function(response) {
     /*optional stuff to do after success */
-    console.log(response)
+    // console.log(response)
     var name,
         description,
         type,
@@ -15,51 +15,64 @@ App.getRandomBeer = function() {
     description = response.data.style.description;
     beerId = response.data.id
 
-    $('.beer__name').html(name);
-    $('.beer__description').html(description);
+    $('.random-beer__name').html(name);
+    $('.random-beer__description').html(description);
 
     // beer style/type
     if( response.data.style.name ){
       type = response.data.style.name;
-      $('.beer__type').html(type);
+      $('.random-beer__type').html(type);
     } else {
-      $('.beer__type').html('Unavailable');
+      $('.random-beer__type').html('Unavailable');
     };
 
     // beer image
     if (response.data.labels) {
       if( response.data.labels.medium ){
         image = response.data.labels.medium;
-        $('.beer__image').attr('src', image);
+        $('.random-beer__image').attr('src', image);
       } else if (response.data.labels.icon){
         image = response.data.labels.icon;
-        $('.beer__image').attr('src', image);
+        $('.random-beer__image').attr('src', image);
       } else if (response.data.labels.large){
         image = response.data.labels.large
-        $('.beer__image').attr('src', image);
+        $('.random-beer__image').attr('src', image);
       } else {
         image = 'http://img1.wikia.nocookie.net/__cb20140409232122/simpsons/images/0/03/Duff_logo.gif'
-        $('.beer__image').attr('src', image);
+        $('.random-beer__image').attr('src', image);
       };
     };
     $.get('http://api.brewerydb.com/v2/beer/'+beerId+'/breweries?key=819b17a216351db6794aaf097f40c86a', function(response) {
       console.log(response);
-      var brewery = response.data[0].name;
-      $('.beer__brewery').html(brewery);
+      var breweryName = response.data[0].name;
+      var breweryId = response.data[0].id;
+      var $breweryTitle = $('.random-beer__brewery');
+      $breweryTitle.html(breweryName);
+      $breweryTitle.attr('href', "/breweries/" + breweryId);
+      localStorage.breweryName = breweryName;
+      localStorage.breweryId = breweryId;
     });
   });
 }
 
-App.getBreweryPage = function(){
-  $.get('http://api.brewerydb.com/v2/beer/'+beerId+'/breweries?key=819b17a216351db6794aaf097f40c86a', function(response) {
+App.buildBeerPage = function(data){
+
+  var beerName,
+      beerDescription,
+      beerType,
+      beerImage,
+      beerId
+
+  beerId = data
+  $.get('http://api.brewerydb.com/v2/beers/'+ beerId +'/?key=819b17a216351db6794aaf097f40c86a', function(response){
     console.log(response);
-    var brewery = response.data[0].name;
-    $('.beer__brewery').html(brewery);
   });
-};
+}
 
 $(document).ready(function(){
   $(".click-me").click(App.getRandomBeer);
-  $(".beer__brewery").click(App.getBreweryPage);
+  if (top.location.pathname === '/beers/' + localStorage.beerId){
+    App.getAllBreweryBeers();
+  };
 });
 
