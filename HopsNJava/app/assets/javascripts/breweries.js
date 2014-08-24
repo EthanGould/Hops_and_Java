889c286e0ec0c0b4f086351ab871ecf9ce7c5ebc
Ethan Gould <ethan.g.907@gmail.com>
2014-08-24 12:57:21 -0400

    built page to display brewery's beers

--- HopsNJava/app/assets/javascripts/breweries.js
+++ HopsNJava/app/assets/javascripts/breweries.js
@@ -1,5 +1,20 @@
 App = App || {};
 
-$(document).ready(function(){
+App.getAllBreweryBeers = function(){
+  var breweryId = localStorage.breweryId
+  $.get('http://api.brewerydb.com/v2/brewery/'+breweryId+'/beers?key=819b17a216351db6794aaf097f40c86a', function(response){
+    console.log(response)
+    for (i = 0; i < response.data.length; i++) {
+      var currentBeer = response.data[i]
+      var $beer = $("<div class='someBeer'><a href='/beers/"+currentBeer.id+"' class='beer__list--item' id="+currentBeer.id+ "'>"+currentBeer.name+"</a></div>");
+      $('.beer__list').append($beer);
+    }
+  });
+}
 
+$(document).ready(function(){
+  $('.brewery__name').html(localStorage.breweryName)
+  if (top.location.pathname === '/breweries/' + localStorage.breweryId){
+    App.getAllBreweryBeers();
+  };
 });
