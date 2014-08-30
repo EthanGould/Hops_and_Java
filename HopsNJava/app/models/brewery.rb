da014d0f095780044fe085c321337408820b75d2
Ethan Gould <ethan.g.907@gmail.com>
2014-08-30 11:31:15 -0400

    add API call to get breweries

--- HopsNJava/app/models/brewery.rb
+++ HopsNJava/app/models/brewery.rb
@@ -1,2 +1,14 @@
 class Brewery < ActiveRecord::Base
+
+  def self.get_breweries
+    breweries_url = "http://api.brewerydb.com/v2/breweries?key=819b17a216351db6794aaf097f40c86a&p=2"
+    breweries_response = HTTParty.get(breweries_url)
+    breweries_response['data'].each do |brewery|
+      Brewery.create!(name: brewery['name'], description: brewery['description'],
+        pic: brewery.try(:[],'images').try(:[], 'icon'),
+        ext_brewery_id: brewery['id'],
+        established: brewery['established'],
+        website: brewery['website'])
+    end
+  end
 end
