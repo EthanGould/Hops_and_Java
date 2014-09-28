316a9de321a5dc849dc4b120dbe635d857d0825a
Ethan Gould <ethan.g.907@gmail.com>
2014-09-27 21:00:14 -0400

    progress on search filter, still WIP

--- HopsNJava/app/assets/javascripts/search.js
+++ HopsNJava/app/assets/javascripts/search.js
@@ -1,20 +1,26 @@
 $(document).ready(function(){
 
-  $('.search-input').keyup(function(event) {
+  $('.search-input').focusin(function(event) {
     event.preventDefault;
     // console.log($(this))
     filterResuts($(this));
   });
 });
 
-var pageItems = $('.list-of-beers').children();
+// var pageItems = $('.list-of-beers').children();
 
 var filterResuts = function(input) {
-  var query = input.val();
-  var userInput = [];
-  userInput += query;
-  for (var i = 0; i < pageItems.length; i++) {
-    console.log(pageItems[i].innerText);
-  }
-  console.log("yo");
-}
+  input.keyup(function(event) {
+    var pageItems = $('.list-of-beers').children();
+    input = $(this).val();
+    console.log($(this).val());
+    for (var i = 0; i < pageItems.length; i++) { //each beer element
+      var beer = pageItems[i].innerText;
+      if (beer.indexOf(input) >= 0) {
+        console.log(pageItems[i]);
+        pageItems.remove(pageItems[i]);
+      }
+    }
+    console.log(pageItems[i]);
+  });
+};
